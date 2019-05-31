8854afc4f4ef3ba8da0497b10c18b5bf63f23748
spozdnyakov <sergey.pozdnyakov@patientkeeper.com>
2019-05-31 16:15:21 -0400

    1 Creating a new Component

---
+++ src/components/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <hi>{{title}}</hi>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'BookList',
+    data(){
+      return{
+        title: "All Books"
+      }
+    }
+  };
+</script>
+
+<style scoped>
+h1, h2{
+  font-weight: normal;
+}
+</style>
